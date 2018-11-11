714aa8e766bc0dd085bf3bb77b391850bc652d7f
al15040 <34472872+al15040@users.noreply.github.com>
2018-11-12 00:29:38 +0900

    Initial commit

---
+++ GrowCut/GrowCut.cpp
@@ -0,0 +1,180 @@
+#include "GrowCut.h"
+
+#include <iostream>
+
+GrowCut::GrowCut(const std::vector<int>& sourceReso, int labelTypeNum)
+{
+	if (labelTypeNum < 1) throw std::exception();
+
+	prohibit_attack_threshold_ = 2 * (int)sourceReso.size(); 
+	force_occupied_threshold_  = 2 * (int)sourceReso.size();
+
+	label_type_num_ = labelTypeNum;
+	for (size_t i = 0; i < sourceReso.size(); ++i)
+		src_resolution_.push_back(sourceReso[i]);
+}
+
+
+GrowCut::GrowCut
+( const std::vector<int>& sourceReso
+, int labelTypeNum
+, int prohibitAttackThres
+, int forceOccupiedThres
+)
+{
+	if (labelTypeNum < 1) throw std::exception();
+
+	prohibit_attack_threshold_ = prohibitAttackThres;
+	force_occupied_threshold_  = forceOccupiedThres;
+
+	label_type_num_ = labelTypeNum;
+	for (size_t i = 0; i < sourceReso.size(); ++i)
+		src_resolution_.push_back(sourceReso[i]);
+}
+
+
+
+GrowCut::~GrowCut()
+{
+}
+
+
+void GrowCut::initState(const short* src, const int* initLabel)
+{
+	src_size_ = 1;
+	for (const auto& reso : src_resolution_)
+		src_size_ *= reso;
+
+	src_state_.clear();
+	src_state_.resize(src_size_);
+#pragma omp parallel for
+	for (int i = 0; i < src_size_; ++i)
+	{
+		src_state_[i] = CellState(initLabel[i], 1.0, EVecXi((int)src[i]));
+		if (initLabel < 0) src_state_[i].strength_ = 0.0;
+	}
+}
+
+
+void GrowCut::Segmentation(const short* src, const int* initLabel, std::vector<int>& result)
+{
+	initState(src, initLabel);
+	result.clear();
+	result.resize(src_size_);
+
+	while( GrowCell() ) std::cout << "Grow Cell\n";
+
+	#pragma omp parallel for
+	for (int i = 0; i < (int)src_state_.size(); ++i)
+		result[i] = src_state_[i].label_;
+}
+
+
+bool GrowCut::GrowCell()
+{
+	bool isChangeState = false;
+
+#pragma omp parallel for
+	for (int i = 0; i < src_size_; ++i) if (src_state_[i].label_ > -2)
+	{
+		if ( isChangeState = IsForceOccupied(i) ) continue;
+
+		//近傍セルが注目セルに攻撃を仕掛ける
+		for ( const auto& neighbor : searchNeighborIdx(i) )
+		{
+			//近傍セルの近傍に敵がたくさんいる場合は攻撃を禁止
+			int weakestEnemyIdx;
+			int maxEnemyNum = calcMaxEnemyNum(neighbor, searchNeighborIdx(neighbor), weakestEnemyIdx);
+			if (maxEnemyNum >= prohibit_attack_threshold_) continue;
+
+			double edge = EvaluationFunc(i, neighbor) * src_state_[neighbor].strength_;
+			if (edge > src_state_[i].strength_)
+			{
+				src_state_[i].label_ = src_state_[neighbor].label_;
+				src_state_[i].strength_ = edge;
+				isChangeState = true;
+			}
+		}
+	}
+
+	return isChangeState;
+}
+
+// ノイマン近傍
+std::vector<int> GrowCut::searchNeighborIdx(int idx)
+{
+	std::vector<int> neighborIndices;
+	for (int i = 0; i < (int)src_resolution_.size(); ++i)
+	{
+		int neighbor  = 1;
+		for (int j = 0; j < i; ++j)
+			neighbor *= src_resolution_[j];
+
+		//変更不可ラベルが定義されているセルは近傍とみなさない
+		if (idx + neighbor < src_size_ && src_state_[idx + neighbor].label_ > -2) 
+			neighborIndices.push_back(idx + neighbor);
+
+		if (idx - neighbor >= 0 && src_state_[idx - neighbor].label_ > -2) 
+			neighborIndices.push_back(idx - neighbor);
+	}
+
+	return neighborIndices;
+}
+
+
+//[0,1]に正規化されており、かつ、色差が大きいほど評価値が小さくなるように関数
+double GrowCut::EvaluationFunc(int idx1, int idx2)
+{
+	return exp( -(src_state_[idx1].feature_vec_ - src_state_[idx2].feature_vec_).norm() );
+}
+
+
+int GrowCut::calcMaxEnemyNum(int idx, const std::vector<int>& neighborIndices, int& weakestEnemyIdx)
+{
+	std::vector<int> enemyNum(label_type_num_, 0);
+	for (const auto& neighbor : neighborIndices)
+	{
+		int neighborLabel = src_state_[neighbor].label_;
+		//近傍ラベルが定義済み　かつ　注目セルのラベルと違う場合
+		if (neighborLabel >= 0 && src_state_[idx].label_ != neighborLabel)
+			enemyNum[neighborLabel]++;
+	}
+
+	int maxEnemyNum = 0;
+	int maxEnemyLabel = 0;
+	for (int label = 0; label < (int)enemyNum.size(); ++label)
+	if (enemyNum[label] > maxEnemyNum)
+	{
+		maxEnemyNum = enemyNum[label];
+		maxEnemyLabel = label;
+	}
+
+	double minStrength = DBL_MAX;
+	for (const int& neighbor : neighborIndices)
+	if (src_state_[neighbor].label_ == maxEnemyLabel && src_state_[neighbor].strength_ < minStrength)
+	{
+		minStrength = src_state_[neighbor].strength_;
+		weakestEnemyIdx = neighbor;
+	}
+	
+	return maxEnemyNum;
+}
+
+
+bool GrowCut::IsForceOccupied(int interestIdx)
+{
+	std::vector<int> neighborIndices = searchNeighborIdx(interestIdx);
+	int weakestEnemyIdx;
+	int maxEnemyNum = calcMaxEnemyNum(interestIdx, neighborIndices, weakestEnemyIdx);
+
+	//注目セルの近傍に多くの敵がいる場合は、最も弱い近傍セルに侵略される
+	if (maxEnemyNum >= force_occupied_threshold_)
+	{
+		double edge = EvaluationFunc(interestIdx, weakestEnemyIdx) * src_state_[weakestEnemyIdx].strength_;
+		src_state_[interestIdx].label_ = src_state_[weakestEnemyIdx].label_;
+		src_state_[interestIdx].strength_ = edge;
+		
+		return true;
+	}
+	return false;
+}
---
+++ GrowCut/GrowCut.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include<vector>
+#include"Eigen/Core"
+
+typedef Eigen::VectorXi EVecXi;
+
+struct CellState
+{
+	int label_; // -1 : undefined, -2 // 変更不可
+	double strength_;
+	EVecXi feature_vec_;
+
+	CellState(int label, double strength, const EVecXi& featureVec)
+	{
+		label_ = label;
+		strength_ = strength;
+		feature_vec_ = featureVec;
+	}
+};
+
+
+class GrowCut
+{
+private:
+	std::vector<int> src_resolution_;
+	std::vector<CellState> src_state_;
+	int src_size_;
+	int label_type_num_;
+
+	int prohibit_attack_threshold_; //攻撃を禁止する閾値
+	int force_occupied_threshold_; //強制的に侵食される閾値
+
+public:
+	GrowCut(const std::vector<int>& sourceReso, int labelTypeNum);
+	GrowCut(const std::vector<int>& sourceReso, int labelTypeNum, int prohibitAttackThres, int forceOccupiedThres);
+	~GrowCut();
+
+	void Segmentation(const short* src, const int* initLabel, std::vector<int>& result);
+private:
+	void initState(const short* src, const int* initLabel);
+	bool GrowCell();
+	std::vector<int> searchNeighborIdx(int idx);
+	double EvaluationFunc(int idx1, int idx2);
+	int calcMaxEnemyNum(int idx, const std::vector<int>& neighborIdx, int& weakestEnemyIdx);
+
+	bool IsForceOccupied(int interestIdx);
+};
